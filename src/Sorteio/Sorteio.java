04d7844c259e0b0ada45ffd13c3806252a340672
Marrowsed <marcus.ah@gmail.com>
2020-09-07 20:52:09 -0300

    Criação do Vetor do sorteio

--- src/Sorteio/Sorteio.java
+++ src/Sorteio/Sorteio.java
@@ -1,5 +1,16 @@
 package Sorteio;
 
 public class Sorteio {
+	
+	public int  v[];
+    public int  size;
+    public int s;
+    
+    Sorteio(int tam) {
+        size = tam;
+        v = new int[size];
+        s = -1;
+    }
+    
 
 }
