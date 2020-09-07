150cef33a7b097969ca1496a90e42b7a10b125b2
Marrowsed <marcus.ah@gmail.com>
2020-09-07 20:57:54 -0300

    Criação do Sorteio

--- src/Sorteio/Sorteio.java
+++ src/Sorteio/Sorteio.java
@@ -1,10 +1,14 @@
 package Sorteio;
 
+import java.util.*;
+
 public class Sorteio {
 	
-	public int  v[];
-    public int  size;
+	public int v[];
+    public int size;
     public int s;
+    
+    Random r = new Random();
     
     Sorteio(int tam) {
         size = tam;
@@ -12,5 +16,41 @@ public class Sorteio {
         s = -1;
     }
     
+    public void insert(int x){
+        s++;
+        v[s] = x;
+    }
+    
+    public int remove(){
+        int temp = v[s];
+        s--;
+        return temp;
+    }
+    
+    public int getS(){
+        return v[s];
+    }
 
+    public boolean isFull(){
+        return s == getSize() - 1;
+    }
+    
+    public boolean isEmpty(){
+        return s == -1;
+    }
+    
+    public int getValue(int pos){
+        return v[pos];
+    }
+
+    public int getSize() {
+        return size;
+    }
+    
+    public void sort() {
+    	for (int i = 0; i < 1; i++) {
+    		int number = r.nextInt( getSize());
+    		System.out.println("O numero sorteado: " + getValue(number));
+    	}
+    }
 }
